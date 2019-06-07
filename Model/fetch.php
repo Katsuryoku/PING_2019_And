1a7788b66f84f88e903694d47114b30731adfe4b
Thomas <urufu.mion@gmail.com>
2019-06-07 10:42:28 +0200

    M12

--- Model/fetch.php
+++ Model/fetch.php
@@ -20,7 +20,7 @@ function fetch($post_view,$bdd, $checkatt, $idorder, $con, $idmana = '',$idUser 
        $update_query = "UPDATE ".$bdd." SET ".$checkatt." = 1 WHERE ".$checkatt."=0 AND Valide =0";
      }else{
         // Cas Manager
-       $update_query = "UPDATE ".$bdd." SET ".$checkatt." = 1 WHERE ".$checkatt."=0 AND Prevalide =0 AND idRespHier = ".$idmana;
+      $update_query = "UPDATE ".$bdd." SET ".$checkatt." = 1 WHERE ".$checkatt."=0 AND (Valide =1 OR  `MotifRefus` IS NOT NULL OR Prevalide =0) AND idRespHier = ".$idmana;
      }
    }
    mysqli_query($con, $update_query); 
@@ -28,14 +28,14 @@ function fetch($post_view,$bdd, $checkatt, $idorder, $con, $idmana = '',$idUser 
  if ($idUser != ''){
   $query =  "SELECT * FROM ".$bdd." WHERE (Valide =1 OR  `MotifRefus` IS NOT NULL) AND ".$checkatt."=0 AND idsalaries = ".$idUser."  ORDER BY ".$idorder." DESC LIMIT 5" ;
 
- }else{
+}else{
    // Cas RH
-   if ($idmana == ''){
-    $query = "SELECT * FROM ".$bdd." WHERE Valide =0 AND ".$checkatt."=0 ORDER BY ".$idorder." DESC LIMIT 5";
-  }else{
+ if ($idmana == ''){
+  $query = "SELECT * FROM ".$bdd." WHERE Valide =0 AND ".$checkatt."=0 ORDER BY ".$idorder." DESC LIMIT 5";
+}else{
     // cas Manager
-    $query = "SELECT * FROM ".$bdd." WHERE  ".$checkatt."=0 AND idRespHier = ".$idmana." AND Prevalide =0 ORDER BY ".$idorder." DESC LIMIT 5";
-  }
+  $query = "SELECT * FROM ".$bdd." WHERE  ".$checkatt."=0 AND idRespHier = ".$idmana." AND (Valide =1 OR  `MotifRefus` IS NOT NULL OR Prevalide =0) ORDER BY ".$idorder." DESC LIMIT 5";
+}
 }
 $result = mysqli_query($con, $query);
 $output = '';
@@ -44,45 +44,68 @@ if(mysqli_num_rows($result)>0)
   while($row = mysqli_fetch_array($result))
   {
     if ($idUser != ''){
+      // User
       if ($row["MotifRefus"] == NULL){
 
-      $output .= '
-      <li>
-      <a href="../View/historique.php">
-      <strong> La demande du '.$row["Date_deb"].' a été validée !</strong><br />
-      </a>
-      </li>
-      ';
+        $output .= '
+        <li>
+        <a href="../View/historique.php">
+        <strong> La demande du '.$row["Date_deb"].' a été validée !</strong><br />
+        </a>
+        </li>
+        ';
       }else{
 
-      $output .= '
-      <li>
-      <a href="../View/historique.php">
-      <strong> La demande du '.$row["Date_deb"].' a été refusée !</strong><br />
-      <small><em> Motif : '.$row["MotifRefus"].'</em></small>
-      </a>
-      </li>
-      ';
+        $output .= '
+        <li>
+        <a href="../View/historique.php">
+        <strong> La demande du '.$row["Date_deb"].' a été refusée !</strong><br />
+        <small><em> Motif : '.$row["MotifRefus"].'</em></small>
+        </a>
+        </li>
+        ';
       }
     }else{
+      // RH
       if ($idmana == ''){
         $output .= '
         <li>
-        <a href="../RH/demandeCours.php">
-        <strong>'.$row["iddemande"].' : '.$row["idsalaries"].'</strong><br />
-        <small><em>'.$row["Date_deb"].'</em></small>
+        <a href="../Manager/demandeCours.php">
+        <strong> Nouvel demande du '.$row["Date_deb"].' de '.$row["idsalaries"].'. </strong><br />
         </a>
         </li>
         ';
       }else{
-        $output .= '
-        <li>
-        <a href="../Manager/demandeCours.php">
-        <strong>'.$row["iddemande"].' : '.$row["idsalaries"].'</strong><br />
-        <small><em>'.$row["Date_deb"].'</em></small>
-        </a>
-        </li>
-        ';
+        // Manager
+        if ($row["Valide"] == 1){
+          $output .= '
+          <li>
+          <a href="../Manager/demandeCours.php">
+          <strong> La demande du '.$row["Date_deb"].' de '.$row["idsalaries"].' a été validée. </strong><br />
+          </a>
+          </li>
+          ';
+        }else{
+          if ($row["MotifRefus"] == NULL){
+            $output .= '
+            <li>
+            <a href="../Manager/demandeCours.php">
+            <strong> Nouvel demande du '.$row["Date_deb"].' de '.$row["idsalaries"].'. </strong><br />
+            </a>
+            </li>
+            ';
+          }else{
+            $output .= '
+            <li>
+            <a href="../Manager/demandeCours.php">
+            <strong> La demande du '.$row["Date_deb"].' de '.$row["idsalaries"].' a été refusée. </strong><br />
+            </a>
+            </li>
+            ';
+          }
+
+        }
+        
 
       }
     }
@@ -99,7 +122,7 @@ if ($idUser != ''){
 
     $status_query = "SELECT * FROM ".$bdd." WHERE ".$checkatt."=0 AND Valide =0";
   }else{
-    $status_query = "SELECT * FROM ".$bdd." WHERE ".$checkatt."=0 AND Prevalide =0 AND idRespHier = ".$idmana;
+    $status_query = "SELECT * FROM ".$bdd." WHERE ".$checkatt."=0 AND (Valide =1 OR  `MotifRefus` IS NOT NULL OR Prevalide =0) AND idRespHier = ".$idmana;
   }
 }
 $result_query = mysqli_query($con, $status_query);
