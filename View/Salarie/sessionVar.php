9385b59240b669068d61db3e6a4baa801054bce9
cliquetgo21 <43533769+cliquetgo21@users.noreply.github.com>
2019-06-12 15:14:33 +0200

    Faire des demandes viables

--- View/Salarie/sessionVar.php
+++ View/Salarie/sessionVar.php
@@ -11,12 +11,15 @@
 	}
 
 	if (isset($_POST["startDate"])) {
-
+		$idtype= $_SESSION["idtype"];
+		
 		$login = $_SESSION["login"];
-		// var_dump($login);
 		$queryS = "SELECT * FROM salarie WHERE login = '".$login."'";
 	    $resultS = mysqli_query($con, $queryS);
 	    $rowS = mysqli_fetch_array($resultS);
+	    $idsalaries = $rowS["idsalaries"];
+		$idRespHier = $rowS["idRespHier"];
+
 	    $deb = $_POST["startDate"];
 	    $end = $_POST["endDate"];
 
@@ -24,47 +27,51 @@
 		$Date_end = strtotime($end);
 		$deb= new DateTime($deb);
 		$deb = $deb->format("Y-m-d");
-		var_dump($deb);
-
-		$idtype= $_SESSION["idtype"];
-		$idsalaries = $rowS["idsalaries"];
-		$idRespHier = $rowS["idRespHier"];
+		// var_dump($deb);
 		$NbEngage = dateDiff($Date_end,$Date_deb);
-		$NbEngage = $NbEngage["day"];
+		$NbEngage = (float)$NbEngage["day"];
 		
-		var_dump($_POST["demiDeb"])	;
-		var_dump($_POST["demiFin"])	;
-		if($_POST["demiDeb"]){
+
+		
+		$demiDeb = $_POST["demiDeb"];
+		$demiFin = $_POST["demiFin"];
+		var_dump($demiDeb);
+		var_dump($demiFin);
+		var_dump($NbEngage);
+		//echo $demiDeb;
+		if($demiDeb=="true" && $demiFin=="true"){
 			$demiJournee = true;
-			if($_POST["demiFin"]){
-				$NbEngage=$NbEngage-1;
-			}else{
-				$NbEngage=$NbEngage-0.5;
-			}
-
-		}else{
-			$demiJournee = false;
-			if($_POST["demiFin"]){
-				$NbEngage=$NbEngage-0.5;
-			}
+			$NbEngage=$NbEngage-1;
 		}
-
+		elseif($demiDeb!="true" && $demiFin=="true"){
+			$demiJournee = false;
+			$NbEngage=$NbEngage-0.5;
+		}
+		elseif($demiDeb=="true" && $demiFin!="true"){
+			$demiJournee = true;
+			$NbEngage=$NbEngage-0.5;
+		}
+		elseif($demiDeb!="true" && $demiFin!="true"){
+			$demiJournee = false;
+		}
+		var_dump($demiJournee);
+		var_dump($NbEngage);
 		
 		
 		// var_dump($NbEngage);
 		$query = "INSERT INTO `demande` (`iddemande`, `idtype`, `idsalaries`, `idRespHier`, `Date_deb`, `demiJournee`, `NbEngage`, `Prevalide`, `Valide`, `MotifRefus`, `idjustif`, `viewByManager`, `viewByRH`, `viewByEmployee`) VALUES (NULL, ".$idtype.", ".$idsalaries.", ".$idRespHier.", '".$deb."', ".$demiJournee.", ".$NbEngage.", '0', '0', NULL, NULL, '0', '0', '0')";
 		var_dump($query);
 		$result = mysqli_query($con, $query);
-/*
-		include "../../Model/sendMailNotif.php";
-		$queryE = "SELECT * FROM demande WHERE iddemande = MAX(iddemande)";
-		$resultE = mysqli_query($con, $query);
-		$rowE = mysqli_fetch_array($resultE);
-		$id = $rowE["iddemande"];
-		sendMailNote($id,"EE");
-*/
 
+		// include "../../Model/sendMailNotif.php";
+		// $queryE = "SELECT * FROM demande WHERE iddemande = MAX(iddemande)";
+		// $resultE = mysqli_query($con, $query);
+		// $rowE = mysqli_fetch_array($resultE);
+		// $id = $rowE["iddemande"];
+		// sendMailNote($id,"EE");
 	}
+
+
 	function dateDiff($date1, $date2){
     $diff = abs($date1 - $date2); // abs pour avoir la valeur absolute, ainsi éviter d'avoir une différence négative
     $retour = array();
